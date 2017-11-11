b15e6f3209ae971b6b0a2ab4ada6e7db416a9e46
Farell <farell@Farells-iMac.local>
2017-11-11 06:30:37 -0800

    Initial Commit

---
+++ TicTacToe/RoomsModel.swift
@@ -0,0 +1,9 @@
+//
+//  RoomsModel.swift
+//  TicTacToe
+//
+//  Created by Farell on 11/11/17.
+//  Copyright © 2017 Farell. All rights reserved.
+//
+
+import Foundation
---
+++ TicTacToe/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  TicTacToe
+//
+//  Created by Farell on 10/11/17.
+//  Copyright © 2017 Farell. All rights reserved.
+//
+
+import Foundation
